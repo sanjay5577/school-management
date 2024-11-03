f788d8bf7df5eca9386e42198274f658f21389a8
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-11-03 17:22:12 +0530

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
           className="flex items-center justify-center lg:justify-start gap-2"
         >
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">St Joseph's School</span>
+          <span className="hidden lg:block">St Joseph&apos;s School</span>
         </Link>
 
         <Menu />
